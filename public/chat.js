96cb35d8329f5436feb018f4a910f47d628e63d6
badog1011 <den2092@gmail.com>
2014-03-14 13:38:00 +0800

    DJ
    API key 尚未拿到!

--- public/chat.js
+++ public/chat.js
@@ -35,7 +35,49 @@ window.onload = function () {
 
 		return false;
 	}
-	
-	
+
 	socket.on('text', addMessage);
+
+	//Play Music
+	var playing = document.getElementById('playing');
+	function play (song) {
+		if (!song) return;
+		playing.innerHTML = '<hr><b>Now Playing: </b> '
+		+ song.ArtistName + ' ' + song.SongName + '<br>'
+
+		var iframe = document.createElement('iframe');
+		iframe.frameborder = 0;
+		iframe.src = song.Url;
+		palying.appendChild(iframe);
+	};
+	socket.on('song', paly);
+
+	//search form
+	var form = document.getElementById('dj');
+	var results = document.getElementById('results');
+	form.stytle.display = 'block';
+	form.onsubmit = function () {
+		results.innerHTML = '';
+		socket.emit('search', document.getElementById('s').value, function (songs) {
+			for (var i = 0; l = songs.length; i < l, i++) {
+				(function (song) {
+					var result = document.createElement('li');
+					result.innerHTML = song.ArtistName + ' - <b>' + song.SongName + '</b> ';
+					var a = document.getElementById('a');
+					a.href = '#';
+					a.innerHTML = function () {
+						socket.emit('song', song);
+						play(song);
+						return false;
+					} 
+					result.appendChild(a);
+					resuls.appendChild(result);
+				})(song[i]);
+			}
+		});
+		return false;
+	};
+	socket.on('elected', function () {
+		form.className = 'isDJ';
+	});
 }
--- server.js
+++ server.js
@@ -7,6 +7,7 @@
 **/
 var express = require('express')
   , sio = require('socket.io')
+  , request = require('superagent')
 
 /**
 *
@@ -21,15 +22,46 @@ app = express.createServer(
 
 app.listen(3000);
 
-var io = sio.listen(app);
+var io = sio.listen(app)
+  , apikey = '{}'//API KEY
+  , currentSong
+  , dj
+
+function elect (socket) {
+	dj = socket;
+	io.sockets.emit('announcement', socket.nickname + ' is the new dj');
+	socket.emit('elected');
+	socket.dj = true;
+	socket.on('disconnect', function () {
+		dj = null;
+		io.sockets.emit('announcement', 'the dj left - next one to join become dj');
+	});
+}
 
 io.sockets.on('connection', function (socket) {
 	socket.on('join', function (name) {
 		socket.nickname = name;
 		socket.broadcast.emit('announcement', name + ' joined the chat');
+		if (!dj) {
+			elect(socket);
+		} else {
+			socket.emit('song', currentSong);
+		}
 	});
 	socket.on('text', function (msg) {
 		socket.broadcast.emit('text', socket.nickname, msg);
+	});
+	socket.on('search', function (q, fn) {
+		request('http://tinysong.com/s/' + encodeURIComponent(q)
+			+ '?key=' + apikey + '&format=json', function (res) {
+				if (200 == res.status) fn(JSON.parse(res.text));
+			});
+	});
+	socket.on('song', function (song) {
+		if (socket.dj) {
+			currentSong = song;
+			socket.broadcast.emit('song', song);
+		}
 	});
 	// console.log('Someone connected');
 });
